2a6a5bde89bf5875e72a2b1bfd8592049ae1c970
Dominique Bashizi <Dominique Bashizi>
2016-05-06 13:37:46 -0400

    Initial commit

---
+++ src/main/java/tiy/webapp/TiyBankingSystemApplication.java
@@ -0,0 +1,12 @@
+package tiy.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiyBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiyBankingSystemApplication.class, args);
+	}
+}
